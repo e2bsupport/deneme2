9433a2bb8ebf4a1443b1e5693d567788d23e9c81
e2bsupport <49477046+e2bsupport@users.noreply.github.com>
2020-04-28 02:58:37 +0300

    Create select.sql

---
+++ select.sql
@@ -0,0 +1,21 @@
+select * from planet_osm_point where place='country'
+
+select osm_id,ST_AsText(way),name from planet_osm_point where place='country' limit 5;
+
+SELECT * from planet_osm_polygon where name LIKE  '%KKTC%';
+SELECT * from planet_osm_polygon where name LIKE  '%- Kıbrıs%';
+SELECT * from planet_osm_polygon where name LIKE  '%Türkiye%';
+SELECT * from planet_osm_polygon where name LIKE  '%Lefkoşa%';
+SELECT * from planet_osm_point where name LIKE  '%Lefkoşa%';
+SELECT * from planet_osm_point where name LIKE  '%Λευκωσία%';
+
+SELECT * from planet_osm_point where osm_id =  '1893015330';
+
+
+
+SELECT * from planet_osm_nodes where id =  '306213529';
+
+SELECT * from planet_osm_point where name LIKE  '%Kıbrıs%';
+SELECT * from planet_osm_roads where name LIKE  '%Kıbrıs%';
+SELECT * from planet_osm_ways where name LIKE  '%- Kıbrıs%';
+SELECT * FROM spatial_ref_sys where srtext LIKE '%Cyprus%';
